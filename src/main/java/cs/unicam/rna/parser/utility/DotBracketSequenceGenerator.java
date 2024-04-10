9a252c26bff54d92e9a10ec2a825075b5cb29e37
Marvin <marvin.sincini@studenti.unicam.it>
2024-04-10 02:37:33 +0200

    correzione dell'algoritmo dot bracket

--- src/main/java/cs/unicam/rna/parser/utility/DotBracketSequenceGenerator.java
+++ src/main/java/cs/unicam/rna/parser/utility/DotBracketSequenceGenerator.java
@@ -1,15 +1,21 @@
 package cs.unicam.rna.parser.utility;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 import cs.unicam.rna.parser.model.RnaMolecule;
 import cs.unicam.rna.parser.model.RnaPair;
 
 public class DotBracketSequenceGenerator extends DotBracketTranslator {
 
     int [] array;
+	Map<Integer, Integer> scopes;
 
-    public String writeSequence(RnaMolecule m) {
-		this.array = new int[m.getLength()];
-		analyze(m, 0, 0);
+    public String writeSequence(RnaMolecule molecule) {
+		this.array = new int[molecule.getLength()];
+		this.scopes = new HashMap<>();
+		analyze(molecule);
         String data = "";
 		for(int i : array) {
 			data += getDbBracket(i);
@@ -17,16 +23,24 @@ public class DotBracketSequenceGenerator extends DotBracketTranslator {
 		return data +"\n\n";
 	}
 
-    private void analyze(RnaMolecule m, int count, int position) {
-		for(RnaPair pair : m.getPairs()) {
-			if(pair.getFirst().getPosition() > position) {
-				position = pair.getSecond().getPosition();
-			} else if(position < pair.getSecond().getPosition()) {
-				count++;
-			}
-			array[pair.getFirst().getPosition()-1] = 1 + (count*2);
-			array[pair.getSecond().getPosition()-1] = 2 + (count*2);
+    private void analyze(RnaMolecule molecule) {
+		for(RnaPair pair : molecule.getPairs()) {
+			int symbol = getSymbol(pair, 0);
+			array[pair.getFirst().getPosition()-1] = 1 + (symbol*2);
+			array[pair.getSecond().getPosition()-1] = 2 + (symbol*2);
 		}
     }
+
+	private int getSymbol (RnaPair pair, int symbol) {
+		Integer scope = scopes.get(symbol);
+		if(scope == null || scope < pair.getFirst().getPosition()
+						 || scope > pair.getSecond().getPosition()) {
+			scopes.put(symbol, pair.getSecond().getPosition());
+			return symbol;
+		}
+		return getSymbol(pair, symbol + 1);
+	}
+
+
     
 }
